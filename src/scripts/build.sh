ea584eb4ca2d1799e3a467ac856fa1bdce488967
Tobias Ranft <coded@ranft.biz>
2024-03-17 14:39:44 +0100

    Add buildprocess of method cards

--- src/scripts/build.sh
+++ src/scripts/build.sh
@@ -23,6 +23,7 @@ mkdir -p "$BUILDPATH/$LANGUAGE/images/cards/event"
 
 echo "Starting $LANGUAGE events"
 
+
 for file in src/cards/event/[0-9][0-9].md; do
   filename=$(basename -- "$file")
   extension="${filename##*.}"
@@ -42,13 +43,18 @@ for file in src/cards/event/[0-9][0-9].md; do
 
   FONTSIZE=$(($FONTSIZE+8))
   magick "$SRCPATH/cards/event/front.png" \
-  -pointsize 50 -fill "#b9b9b9" -font "build/static/OpenSans-SemiBold.ttf" -draw "text 130,180 '$CATEGORY'" \
+  -pointsize 50 -fill "#ADADAD" -font "build/static/OpenSans-SemiBold.ttf" -draw "text 130,180 '$CATEGORY'" \
   -pointsize 40 -font "build/static/OpenSans-Regular.ttf" -draw "text 1020,100 '$cardnumber'" \
   -fill black -size 930x -pointsize $FONTSIZE caption:"$TEXT" -geometry +120+220\
   -composite "$BUILDPATH/$LANGUAGE/images/cards/event/$cardnumber.png"
 #  -fill black -size 910x pango:"<span font_family=\"Open Sans\" font=\"$FONTSIZE\">$TEXT</span>" -geometry +130+230 \
    #+antialias
 done
+
+magick "$SRCPATH/cards/event/back.png" \
+ -pointsize 100 -fill "#ADADAD" -font "build/static/OpenSans-SemiBold.ttf" -draw "text 220,300 '$CATEGORY'" \
+ "$BUILDPATH/$LANGUAGE/images/cards/event/back.png"
+
 
 # METHODS
 CATEGORY="$(cat "$SRCPATH/cards/method/back.$LANGUAGE.md")"
@@ -76,10 +82,14 @@ for file in src/cards/method/[0-9][0-9].md; do
 
   FONTSIZE=$(($FONTSIZE+8))
   magick "$SRCPATH/cards/method/front.png" \
-  -pointsize 50 -fill "#b9b9b9" -font "build/static/OpenSans-SemiBold.ttf" -draw "text 130,180 '$CATEGORY'" \
+  -pointsize 50 -fill white -font "build/static/OpenSans-SemiBold.ttf" -draw "text 130,180 '$CATEGORY'" \
   -pointsize 40 -font "build/static/OpenSans-Regular.ttf" -draw "text 1020,100 '$cardnumber'" \
   -fill black -size 930x -pointsize $FONTSIZE caption:"$TEXT" -geometry +120+220\
   -composite "$BUILDPATH/$LANGUAGE/images/cards/method/$cardnumber.png"
 #  -fill black -size 910x pango:"<span font_family=\"Open Sans\" font=\"$FONTSIZE\">$TEXT</span>" -geometry +130+230 \
    #+antialias
 done
+
+magick "$SRCPATH/cards/method/back.png" \
+ -pointsize 100 -fill white -font "build/static/OpenSans-SemiBold.ttf" -draw "text 220,300 '$CATEGORY'" \
+ "$BUILDPATH/$LANGUAGE/images/cards/method/back.png"
